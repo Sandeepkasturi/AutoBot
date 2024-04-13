82e69849c461be43f1d071a22645c818ea2ec3b9
Sandeep Kasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2024-04-14 01:18:08 +0530

    Streamlit Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,15 +31,18 @@ def download_generated_code(content, filename='generated_code.txt'):
 # Main Streamlit application
 def main():
     st.title("Auto BOT")
+    st.image("auto_bot_1.png")
     st.markdown("""
-    Streamlit-powered SKAV TECH AI:
-     Functionalities:
+    SKAV TECH presents Auto Bot AI:""")
+    st.markdown("""Functionalities:
      1. Extracts HTML code from a URL.
      2. Generates responses using SKAV AI Chatbot.
+     3. Available for Downloading code files.
 
      Auto BOT is for educational purposes only.
      We are not responsible for any illegal and unethical activities done by You.
     """)
+    st.markdown("Get API key from Google AI Studio")
 
     # Prompt user for API key
     api_key = st.text_input("Enter your Generative AI API key:")
@@ -52,6 +55,25 @@ def main():
         # Create a Generative Model instance (assuming 'gemini-pro' is a valid model)
         model = GenerativeModel('gemini-pro')
 
+        
+        # AI Chatbot section
+        st.header("Auto Bot Chatbot")
+        question = st.text_input("Ask the model a question:")
+        if st.button("Ask AI"):
+            # Call your AI model and get the response
+            response = model.generate_content(question)
+            st.text("Auto Bot Response:")
+            st.write(response.text)
+
+            # Check if the response contains a URL
+            if "http" in response.text:
+                st.write("The response contains a URL.")
+
+            # Check if the question is related to generating code
+            code_keywords = ["code", "write code", "develop code", "generate code"]
+            if any(keyword in question.lower() for keyword in code_keywords):
+                download_generated_code(response.text)
+                
         # HTML Extraction section
         st.header("Extract HTML Code")
         url = st.text_input("Enter URL:")
@@ -65,35 +87,19 @@ def main():
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
 
-        # AI Chatbot section
-        st.header("SKAV AI Chatbot")
-        question = st.text_input("Ask the model a question:")
-        if st.button("Ask SKAV AI"):
-            # Call your AI model and get the response
-            response = model.generate_content(question)
-            st.text("SKAV AI Response:")
-            st.write(response.text)
-
-            # Check if the response contains a URL
-            if "http" in response.text:
-                st.write("The response contains a URL.")
-
-            # Check if the question is related to generating code
-            code_keywords = ["code", "write code", "develop code", "generate code"]
-            if any(keyword in question.lower() for keyword in code_keywords):
-                download_generated_code(response.text)
-
     # Additional buttons
     st.markdown('---')
     st.markdown("&copy; 2024 - All rights reserved SKAV TECH.")
     st.subheader('Our recent projects')
-    button_col3, button_col4 = st.columns(2)
+    button_col3, button_col4, button_col5 = st.columns(3)
 
     if button_col3.button('DataVerse AI'):
         webbrowser.open("https://dataverse-ai.vercel.app/")
 
     if button_col4.button('Ulink'):
         webbrowser.open("https://ulink-io.vercel.app")
+    if button_col5.button('SKAV TECH'):
+        webbrowser.open("https://skavtech.mydurable.com")
 
 
 if __name__ == "__main__":
